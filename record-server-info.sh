d8a14b958b92a64e06ab1483ef098cf96c4f85c4
Tom Coleman <tom@thesnail.org>
2015-06-23 11:10:35 +1000

    Update

--- record-server-info.sh
+++ record-server-info.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-APP=verso
+APP=$1
+USER=$2
+PASS=$3
 GCONTROL=/usr/local/lib/python2.7/dist-packages/gcontrol/git-shell-commands/gcontrol
 
 MACHINES=$(sudo -H -u $APP $GCONTROL inventory | grep MeteorApp)
@@ -10,5 +12,6 @@ while read -r MACHINE; do
   IP=$(echo "$MACHINE" | cut -f 1 -d ' ')
   NAME=$(echo "$MACHINE" | cut -f 2 -d '/' | cut -f 1 -d ")")
 
-  echo "$IP -> $NAME"
+  OUTPUT=$(curl "http://$USER:$PASS@$IP/info")
+  echo $OUTPUT
 done <<< "$MACHINES"
